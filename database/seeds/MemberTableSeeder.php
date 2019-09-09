9a3bf384546ed0127396f185ef6de31f6012f6d3
programmerPatient <494429797@qq.com>
2019-09-09 13:51:58 +0800

    会员建表

---
+++ database/seeds/MemberTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class MemberSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //使用faker方法模拟多条数据,实例化时，create里面的参数表示本地化
+        $faker = \Faker\Factory::create('zh_CN');
+        $data = [];
+
+        for($i=0;$i<500;$i++){
+            $data[] = [
+                'username' => $faker -> username,
+                'password' => bcrypt('password'),
+                'gender' => rand(1,3),
+                'mobile' => $faker ->phoneNumber,
+                'email' =>$faker -> email,
+                'avatar' => '/images/avatar/th.jpg',
+                'created_at' => date('Y:m:d H:i:s'),
+                'type' => rand(1,2),
+                'status' => rand(1,2),
+            ];
+        }
+        //写入数据表
+        DB::table('member') -> insert($data);
+    }
+}
