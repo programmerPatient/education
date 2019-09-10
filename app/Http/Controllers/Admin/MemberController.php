8e5b056b2faf629701ebbb1d174391a3d684f7ec
programmerPatient <494429797@qq.com>
2019-09-10 19:48:24 +0800

    实现会员列表的展示

--- app/Http/Controllers/Admin/MemberController.php
+++ app/Http/Controllers/Admin/MemberController.php
@@ -4,7 +4,15 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Admin\Member;
 
 class MemberController extends Controller
 {
+    //列表展示
+    public function index(){
+        //查询数据
+        $data = Member::all();
+        //展示视图
+        return view('admin.member.index',compact('data'));
+    }
 }
